576d65ba86c6066c5242cfe23e0b7e82b01e3a7b
alexhaag <alexhaag@hotmail.com>
2026-02-04 00:28:50 -0300

    vite.config 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,21 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  // Diz ao Vite que o projeto frontend está em /client
   root: path.resolve(__dirname, "client"),
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
+
   build: {
+    // outDir SEM path absoluto
+    // sempre relativo ao `root`
     outDir: "dist",
     emptyOutDir: true,
   },
